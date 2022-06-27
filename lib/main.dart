22566ff65eeaa16aaa9c372376ab3afd470d213a
Tom Bradley <me@tomjbradley.co.uk>
2022-06-27 20:35:28 +0100

    Adds save/unsave functionality

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 
@@ -5,11 +6,9 @@ void main() {
   runApp(const MyApp());
 }
 
-// #docregion MyApp
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // #docregion build
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -27,7 +26,6 @@ class MyApp extends StatelessWidget {
       },
     );
   }
-  // #enddocregion build
 }
 
 class FirstScreen extends StatelessWidget {
@@ -55,8 +53,31 @@ class FirstScreen extends StatelessWidget {
   }
 }
 
-class SecondScreen extends StatelessWidget {
-  const SecondScreen({Key? key}) : super(key: key);
+class SecondScreen extends StatefulWidget {
+  const SecondScreen({super.key});
+
+  @override
+  State<SecondScreen> createState() => _SecondScreenState();
+}
+
+class _SecondScreenState extends State<SecondScreen> {
+  final _biggerFont = const TextStyle(fontSize: 18);
+  List<String> names = [];
+
+  @override
+  void initState() {
+    _getNames();
+    super.initState();
+  }
+
+  void _getNames() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    List<String>? response = prefs.getStringList("saved_names");
+
+    setState(() {
+      names = response ?? [];
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -65,48 +86,99 @@ class SecondScreen extends StatelessWidget {
         title: const Text('Saved suggestions'),
         centerTitle: true,
       ),
-      body: const Center(
-        child: Text("Second Screen"),
+      body: Center(
+        child: ListView(children: [
+          for (var name in names)
+            ListTile(
+              title: Text(name),
+            ),
+        ]),
       ),
     );
   }
 }
 
-// #docregion RWS-var
 class _RandomWordsState extends State<RandomWords> {
   final _suggestions = <WordPair>[];
   final _biggerFont = const TextStyle(fontSize: 18);
-  // #enddocregion RWS-var
+  List<String> _savedNames = <String>[];
 
-  // #docregion RWS-build
+  @override
+  void initState() {
+    _getNames();
+    super.initState();
+  }
+
+  void _getNames() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    List<String>? response = prefs.getStringList("saved_names");
+
+    setState(() {
+      _savedNames = response ?? [];
+    });
+  }
+
+  _saveName(String name) async {
+    setState(() {
+      _savedNames.add(name);
+
+      _savedNames = _savedNames;
+    });
+
+    final prefs = await SharedPreferences.getInstance();
+
+    await prefs.setStringList('saved_names', _savedNames);
+  }
+
+  _removeName(String name) async {
+    setState(() {
+      _savedNames.remove(name);
+
+      _savedNames = _savedNames;
+    });
+
+    final prefs = await SharedPreferences.getInstance();
+
+    await prefs.setStringList('saved_names', _savedNames);
+  }
+
   @override
   Widget build(BuildContext context) {
-    // #docregion itemBuilder
     return ListView.builder(
       padding: const EdgeInsets.all(16.0),
-      itemBuilder: /*1*/ (context, i) {
-        if (i.isOdd) return const Divider(); /*2*/
+      itemBuilder: (context, i) {
+        if (i.isOdd) return const Divider();
 
-        final index = i ~/ 2; /*3*/
+        final index = i ~/ 2;
         if (index >= _suggestions.length) {
-          _suggestions.addAll(generateWordPairs().take(10)); /*4*/
+          _suggestions.addAll(generateWordPairs().take(10));
         }
-        // #docregion listTile
+
         return ListTile(
           title: Text(
             _suggestions[index].asPascalCase,
             style: _biggerFont,
           ),
+          trailing: IconButton(
+            icon: (!_savedNames.contains(_suggestions[index].asPascalCase))
+                ? Icon(Icons.favorite_outline)
+                : Icon(
+                    Icons.favorite,
+                    color: Colors.pink,
+                  ),
+            onPressed: () {
+              if (_savedNames.contains(_suggestions[index].asPascalCase)) {
+                _removeName(_suggestions[index].asPascalCase);
+              } else {
+                _saveName(_suggestions[index].asPascalCase);
+              }
+            },
+          ),
         );
-        // #enddocregion listTile
       },
     );
-    // #enddocregion itemBuilder
   }
-  // #enddocregion RWS-build
-  // #docregion RWS-var
 }
-// #enddocregion RWS-var
 
 class RandomWords extends StatefulWidget {
   const RandomWords({super.key});
